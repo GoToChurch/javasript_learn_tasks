b96097bcfae2ece0e05e6e21a1f2b2232680ae2f
Nickich <pravoslavie1488@gmail.com>
2023-03-01 11:57:14 +0300

    Tasks from chapters 8 and 10 added

---
+++ 8/8.2/Task 2. Create_object_from_existing.js
@@ -0,0 +1,7 @@
+function Obj() {
+    console.log("HI")
+
+}
+
+let obj = new Obj();
+let obj2 = new obj.constructor();
---
+++ 8/8.3/Task 2. Add_defer_wrap_to_function_prototype.js
@@ -0,0 +1,13 @@
+function f(a, b) {
+    console.log(a + b);
+}
+
+Function.prototype.defer = function(ms) {
+    let func = this;
+
+    return function(...args) {
+        setTimeout(() => func.apply(this, args), ms);
+    }
+}
+
+f.defer(1000)(1, 2)
